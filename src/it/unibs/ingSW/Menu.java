57fa75a3e274383b04bb6cb9b1ba254c148f4b4b
glisenti.mirko <glisenti.mirko@blninformatica.it>
2021-06-07 23:17:12 +0200

    Aggiunto check directory

--- src/it/unibs/ingSW/Menu.java
+++ src/it/unibs/ingSW/Menu.java
@@ -1,10 +1,13 @@
 package it.unibs.ingSW;
 
+import java.io.File;
+
 import it.unibs.fp.mylib.*;
 
 public class Menu 
 {
 	public static void main(String[] args) throws Exception{
+		checkDirectory();
 		int scelta = 0;
 		do 
 		{
@@ -25,6 +28,22 @@ public class Menu
 			}
 		} 
 		while (scelta != 0);
+	}
+	
+	private static void checkDirectory() {
+		File directory = new File("./salvataggi");
+		File directoryN = new File("./salvataggi/retiN");
+		File directoryPN = new File("./salvataggi/retiPN");
+		
+		if (!directory.isDirectory()) {
+			directory.mkdir();
+			if(!directoryN.isDirectory()) {
+				directoryN.mkdir();
+			}
+			if(!directoryPN.isDirectory()) {
+				directoryPN.mkdir();
+			}
+		}
 	}
 	
 	public static void reti(){
